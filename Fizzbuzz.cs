5de3c7822617c90135d244ef0974062c32a3a649
malenabou <56851295+malenabou@users.noreply.github.com>
2020-06-01 22:01:49 -0300

    Add files via upload

---
+++ Fizzbuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            //We will print number starting from 1 to 100. When a number is multiple of three,
+            //print “Fizz” instead of a number on the console and if multiple of five then print “Buzz” on the console. 
+            //For numbers which are multiple of three as well five, print “FizzBuzz” on the console.
+
+                      Console.WriteLine("Hello World!");
+        }
+    }
+}
